ffd3ffffc430382ec443728300bd27aff1f1b02e
AJakif <akifahmedjahin@gmail.com>
2021-11-09 16:43:52 +0600

    Frequency - Repository Pattern Implemented

--- Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
+++ Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -15,13 +15,12 @@ namespace Uplift.DataAccess.Data.Repository.IRepository
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             string includeProperties = null
             );
-
         T GetFirstOrDefault(
             Expression<Func<T, bool>> filter = null,
             string includeProperties = null
             );
 
-        void Add(T entity);
+        void add(T entity);
 
         void Remove(int id);
         void Remove(T entity);
