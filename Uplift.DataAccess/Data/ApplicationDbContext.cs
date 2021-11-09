ec49a8399a3634c7d9dc34de8952c19c10b1f2d9
AJakif <akifahmedjahin@gmail.com>
2021-11-09 12:16:47 +0600

    Adding Frequency Model to Db

--- Uplift.DataAccess/Data/ApplicationDbContext.cs
+++ Uplift.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace Uplift.DataAccess.Data
         }
 
         public DbSet<Category> Category { get; set; }
+        public DbSet<Frequency> Frequency { get; set; }
     }
 }
---
+++ Uplift.Models/Frequency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+    public class Frequency
+    {
+        [Key] //Attribute
+        public int Id { get; set; } /* Public = access modifier,
+                                        int = Data Type
+                                        Id = data name
+                                        get,set = methods
+                                     */
+        [Required]
+        public int FrequencyCount { get; set; }
+
+        [Required]
+        [Display(Name = "Frequency Name")] // Data name display on client side
+
+        public string Name { get; set; }
+
+    }
+}
