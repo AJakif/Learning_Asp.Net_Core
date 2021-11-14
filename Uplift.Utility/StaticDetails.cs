f86be6059cc732a6dbf800631a2f45b553d7a7ad
AJakif <akifahmedjahin@gmail.com>
2021-11-14 16:11:26 +0600

    Shopping Order and Cart done

--- Uplift.Utility/StaticDetails.cs
+++ Uplift.Utility/StaticDetails.cs
@@ -7,5 +7,8 @@ namespace Uplift.Utility
     public static class StaticDetails
     {
         public const string SessionCart = "Cart";
+        public const string StatusSubmitted = "Submitted";
+        public const string StatusApproved = "Approved";
+        public const string StatusRejected = "Rejected";
     }
 }
--- Uplift/Areas/Customer/Controllers/CartController.cs
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Uplift.DataAccess.Data.Repository;
+using Uplift.DataAccess.Data.Repository.IRepository;
 using Uplift.Extensions;
 using Uplift.Models;
 using Uplift.Models.ViewModels;
@@ -14,12 +15,12 @@ namespace Uplift.Areas.Customer.Controllers
     [Area("Customer")]
     public class CartController : Controller
     {
-        private readonly UnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
 
         [BindProperty]
         public CartViewModel CartVM { get; set; }
 
-        public CartController(UnitOfWork unitOfWork)
+        public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
             CartVM = new CartViewModel()
@@ -40,6 +41,83 @@ namespace Uplift.Areas.Customer.Controllers
                 }
             }
             return View(CartVM);
+        }
+
+        public IActionResult Summary()
+        {
+            if (HttpContext.Session.GetObject<List<int>>(StaticDetails.SessionCart) != null)
+            {
+                List<int> sessionList = new List<int>();
+                sessionList = HttpContext.Session.GetObject<List<int>>(StaticDetails.SessionCart);
+                foreach (int serviceId in sessionList)
+                {
+                    CartVM.ServiceList.Add(_unitOfWork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
+                }
+            }
+            return View(CartVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Summary")]
+        public IActionResult SummaryPOST()
+        {
+            if (HttpContext.Session.GetObject<List<int>>(StaticDetails.SessionCart) != null)
+            {
+                List<int> sessionList = new List<int>();
+                sessionList = HttpContext.Session.GetObject<List<int>>(StaticDetails.SessionCart);
+                CartVM.ServiceList = new List<Service>();
+                foreach (int serviceId in sessionList)
+                {
+                    CartVM.ServiceList.Add(_unitOfWork.Service.Get(serviceId));
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(CartVM);
+            }
+            else
+            {
+                CartVM.OrderHeader.OrderDate = DateTime.Now;
+                CartVM.OrderHeader.Status = StaticDetails.StatusSubmitted;
+                CartVM.OrderHeader.ServiceCount = CartVM.ServiceList.Count;
+                _unitOfWork.OrderHeader.Add(CartVM.OrderHeader);
+                _unitOfWork.Save();
+
+                foreach (var item in CartVM.ServiceList)
+                {
+                    OrderDetails orderDetails = new OrderDetails
+                    {
+                        ServiceId = item.Id,
+                        OrderHeaderId = CartVM.OrderHeader.Id,
+                        ServiceName = item.Name,
+                        Price = item.Price
+                    };
+
+                    _unitOfWork.OrderDetails.Add(orderDetails);
+                    
+                }
+                _unitOfWork.Save();
+
+                HttpContext.Session.SetObject(StaticDetails.SessionCart, new List<int>());
+                return RedirectToAction("OrderConfirmation", "Cart", new { id = CartVM.OrderHeader.Id });
+            }
+        }
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            return View(id);
+        }
+
+        public IActionResult Remove(int serviceId)
+        {
+            List<int> sessionList = new List<int>();
+            sessionList = HttpContext.Session.GetObject<List<int>>(StaticDetails.SessionCart);
+            sessionList.Remove(serviceId);
+            HttpContext.Session.SetObject(StaticDetails.SessionCart, sessionList);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
